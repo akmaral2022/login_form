94e4473a0ae49f775c5cf0d6218a41d988cc5fdb
Akmaral <akmaral.minbaeva1921@gmail.com>
2023-11-30 19:35:55 +0600

    fourth commit

---
+++ src/components/Button/MyButton.jsx
@@ -0,0 +1,6 @@
+import './MyButton.css'
+export const MyButton = (props) => {
+    return (
+        <button className="MyButton" {...props}></button>
+    )
+}
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,16 @@
+import { MyButton } from "../Button/MyButton"
 import { MyInput } from "../Input/MyInput"
+import './Form.css'
 
 export const CustomForm = () => {
     return (
 
-        <div className="form">
+        <div className="form_login">
             <h1>Welcome Back!</h1>
             <MyInput placeholder='Введите логин' />
-            <MyInput placeholder='Введите пароль' />
-
+            <MyInput placeholder='Введите пароль' type='password' />
+            <MyButton>Войти</MyButton>
+            <a href="https://www.example.com">У меня ёще нет аккуанта</a>
         </div>
     )
 }
