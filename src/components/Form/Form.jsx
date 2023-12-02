32357acf66ec75f91db110a9a47fa7997a20f594
Akmaral <akmaral.minbaeva1921@gmail.com>
2023-12-03 03:55:24 +0600

    fifth commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,23 @@
 import { MyButton } from "../Button/MyButton"
 import { MyInput } from "../Input/MyInput"
 import './Form.css'
-import Hide from '../../img/Hide.png'
+import { useState } from "react"
 
 export const CustomForm = () => {
-    return (
 
+    const [passwordItem, setPasswordItem] = useState('')
+    function passwordChange(e) {
+        setPasswordItem(e.target.value.split('').map(() => '*').join(''))
+    }
+
+    return (
         <div className="form_login">
             <h1>Welcome Back!</h1>
             <MyInput placeholder='Введите логин' />
-            <MyInput placeholder='Введите пароль' type='password' />
+            <MyInput placeholder='Введите пароль' type='password' value={passwordItem} onChange={passwordChange} />
             <MyButton>Войти</MyButton>
             <a href="https://www.example.com">У меня ёще нет аккуанта</a>
         </div>
     )
+
 }
