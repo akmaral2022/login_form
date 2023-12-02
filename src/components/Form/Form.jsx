e835030ecff265bc94546f8e8884786021c90072
Akmaral <akmaral.minbaeva1921@gmail.com>
2023-12-03 04:10:51 +0600

    fifth commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,17 +4,21 @@ import './Form.css'
 import { useState } from "react"
 
 export const CustomForm = () => {
-
-    const [passwordItem, setPasswordItem] = useState('')
-    function passwordChange(e) {
-        setPasswordItem(e.target.value.split('').map(() => '*').join(''))
+    const [loginValue, setLigonValue] = useState('')
+    function isLogin(e) {
+        const value = e.target.value
+        const regExpLogin = /^[\w.]{0,25}@gmail\.com$/
+        if (regExpLogin.test(value)) {
+            console.log('Finaly');
+        } console.log('Wrong');
+        setLigonValue(value)
     }
 
     return (
         <div className="form_login">
             <h1>Welcome Back!</h1>
-            <MyInput placeholder='Введите логин' />
-            <MyInput placeholder='Введите пароль' type='password' value={passwordItem} onChange={passwordChange} />
+            <MyInput placeholder='Введите логин' onChange={isLogin} value={loginValue}></MyInput>
+            <MyInput placeholder='Введите пароль' type='password' />
             <MyButton>Войти</MyButton>
             <a href="https://www.example.com">У меня ёще нет аккуанта</a>
         </div>
