b546193e2c09a41a79e72d523d7ea0db0476de64
Akmaral <akmaral.minbaeva1921@gmail.com>
2023-12-05 03:08:01 +0600

    fifth commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,32 +5,42 @@ import { useState } from "react"
 
 export const CustomForm = () => {
     const [loginValue, setLigonValue] = useState('')
+    const [loginError, setLoginError] = useState('')
     const [passwordValue, setPassworValue] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+    const regExpLogin = /^[\w.]{0,25}@gmail\.com$/
+    const regExpPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}/
+
+
     function isLogin(e) {
         const value = e.target.value
         setLigonValue(value)
-        const regExpLogin = /^[\w.]{0,25}@gmail\.com$/
-        if (regExpLogin.test(value)) {
-            console.log('Finaly');
-        } console.log('Wrong');
     }
 
     function isPassword(e) {
         const password = e.target.value
-        const regExpPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}/
         setPassworValue(password)
-        if (regExpPassword.test(password)) {
-            console.log('Good password');
-        } console.log('Too short');
     }
+
+    function checkData() {
+        if (regExpLogin.test(loginValue) && regExpPassword.test(passwordValue)) {
+            console.log('Принято')
+        } else if (!regExpLogin.test(loginValue)) {
+            console.log('Неверно указан логин');
+        } else if (!regExpPassword.test(passwordValue)) {
+            console.log('Неверно указан пароль');
+        }
+
+    }
+
 
     return (
         <div className="form_login">
             <h1>Welcome Back!</h1>
             <MyInput placeholder='Введите логин' value={loginValue} onChange={isLogin}></MyInput>
             <MyInput placeholder='Введите пароль' type='password' value={passwordValue} onChange={isPassword} />
-            <MyButton>Войти</MyButton>
-            <a href="https://www.example.com">У меня ёще нет аккуанта</a>
+            <MyButton onClick={checkData}>Войти</MyButton>
+            <a href="https://www.example.com">У меня ещё нет аккаунта</a>
         </div>
     )
 
