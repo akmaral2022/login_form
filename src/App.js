a94ec5b72448bba2670a15a61b3fd0ff80c495fb
Akmaral <akmaral.minbaeva1921@gmail.com>
2023-12-07 08:24:22 +0600

    fifth commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import { CustomForm } from './components/Form/Form';
 
 function App() {
   const [errorMessage, setErrorMessage] = useState(null);
+  const [formSubmited, setFormSubmited] = useState(false)
 
   const handleFormError = (error) => {
     setErrorMessage(error);
+    console.log(error);
   };
   const handleFormSuccess = () => {
     setErrorMessage(null)
+    setFormSubmited(true)
   }
 
   return (
     <div className="App">
-      <div className='block Poster'>
-      </div>
-      <div className='block Form'>
-        <CustomForm onError={handleFormError} onSuccess={handleFormSuccess} />
-        {errorMessage && <div className="error">{errorMessage}</div>}
-      </div>
+      {formSubmited ?
+        <div className={`block Poster ${formSubmited ? `centered` : ``}`}>
+          <h1>С возвращением!</h1>
+          <a href="" className='go_out'>Выйти</a>
+        </div> :
+        <>
+          <div className={`block Poster ${formSubmited ? `centered` : ``}`}>
+          </div>
+          <div className='block Form'>
+            <CustomForm onError={handleFormError} onSuccess={handleFormSuccess} />
+            {errorMessage && <div className="error">{errorMessage}</div>}
+          </div>
+        </>
+      }
+
     </div>
   );
 }
