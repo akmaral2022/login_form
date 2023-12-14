e2b50440097535b4ac9051ac1d77c632c07850a2
Akmaral <akmaral.minbaeva1921@gmail.com>
2023-12-15 02:07:31 +0600

    fifth commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { useState } from "react"
+
+export function Login() {
+    const [User, setUser] = useState({
+        userLigon: '',
+        userPassword: ''
+    })
+    return (
+        <div></div>
+    )
+}
