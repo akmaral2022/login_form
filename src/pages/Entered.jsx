6029b1993275ac4670b5d004613aae9b3fbce67e
Akmaral <akmaral.minbaeva1921@gmail.com>
2024-01-16 20:26:05 +0600

    fifth commit

--- src/pages/Entered.jsx
+++ src/pages/Entered.jsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom"
 import Frame from "../img/Frame.png"
+import "../moduls.css/LoginPage_modules.css"
+
 
 export const Entered = () => {
     const navigate = useNavigate()
     return (
         <>
-            <div className="block Poster centered">
+            <div className="enter__block">
                 <h1>С возвращением!</h1>
+                <p>Lorby - твой личный репетитор</p>
                 <img src={Frame} alt="Stay at home" />
-                <p className='go_out' onClick={() => navigate('/')}>Выйти</p>
+                <button className='go_out' onClick={() => navigate('/')}>Выйти</button>
             </div>
         </>
     )
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
 import { LoginForm } from "../components/LoginForm/LoginForm"
 import Frame from "../img/Frame.png"
 import "../moduls.css/LoginPage_modules.css"
+import { Entered } from "./Entered";
 
 export const LoginPage = () => {
+
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [formSubmited, setFormSubmited] = useState(false)
+
+    const handleFormError = (error) => {
+        setErrorMessage(error);
+        console.log(error);
+    };
+    const handleFormSuccess = () => {
+        setErrorMessage(null)
+        setFormSubmited(true)
+
+    }
+
     return (
         <div className="login__page">
-            <div className="login__poster">
-                <img src={Frame} alt="Stay at home" />
-                <h1>Lorby</h1>
-                <h3>Твой личный репетитор</h3>
-            </div>
-            <div className="login__place">
-                <LoginForm />
-            </div>
+            {formSubmited ?
+                <Entered /> :
+                <>
+                    <div className="login__poster">
+                        <img src={Frame} alt="Stay at home" />
+                        <h1>Lorby</h1>
+                        <h3>Твой личный репетитор</h3>
+                    </div>
+                    <div className="login__place">
+                        <div className={`block Poster ${formSubmited ? `centered` : ``}`}>
+                        </div>
+                        <div className='block Form'>
+                            <LoginForm onError={handleFormError} onSuccess={handleFormSuccess} />
+                            {errorMessage && <div className="error">{errorMessage}</div>}
+                        </div>
+                    </div>
+                </>
+            }
         </div>
     )
 
